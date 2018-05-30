2db843d9eb51cd969ca00c0ab999c837b20c0baa
boikov <boikov@samsonpost.ru>
2018-05-30 17:02:38 +0300

    Добавил несколько задач и условий для новых

---
+++ adaptive/5.6-comprehensions.py
@@ -0,0 +1,15 @@
+"""
+Не используя интерпретатор, отметьте все однозначно неправильные утверждения 
+о содержимом переменных после выполнения этого кода:
+
+dict1 = dict(zip(('a', 'b', 'c'), (1, 2, 3)))
+range5 = list(range(5))
+lst1 = [i 
+        for i in range5 
+        if i in dict1]
+dict2 = {i: i*i for i in range5}
+lst2 = [dict1[value] for value in dict1]
+lst3 = [i 
+        for i in range5 
+        if i in lst2]
+"""
